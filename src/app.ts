403971cc1a5cc77c1c4c123299a4d33a12b7dd2c
Macbeth98 <manichandra.teja@gmail.com>
2023-11-09 03:36:55 -0500

    feat: added rate-limiter support

--- src/app.ts
+++ src/app.ts
@@ -5,8 +5,8 @@ import fastifyCors from '@fastify/cors';
 import fastifyCompress from '@fastify/compress';
 import { TypeBoxTypeProvider } from '@fastify/type-provider-typebox';
 import fastifyJwt from '@fastify/jwt';
-import { envOptions } from '@config';
 import fastifyEnv from '@fastify/env';
+import fastifyRateLimit from '@fastify/rate-limit';
 
 import { initializeRoutes } from '@plugins/initializeRoute';
 import { authentication } from '@plugins/authentication';
@@ -14,6 +14,7 @@ import { initSwagger } from '@plugins/swagger';
 import mongodbPlugin from '@plugins/mongodb';
 
 import { schemaErrorFormatter } from '@utils/schemaErrorFormatter';
+import { envOptions } from '@config';
 
 class App {
   public app: FastifyInstance;
@@ -65,6 +66,11 @@ class App {
   private async initializePlugins() {
     await this.app.register(fastifyEnv, envOptions);
     await this.app.register(mongodbPlugin, { uri: this.app.config.DATABASE_URL });
+    await this.app.register(fastifyRateLimit, {
+      max: 60,
+      timeWindow: '1 minute',
+      allowList: ['127.0.0.1']
+    });
     this.app.register(fastifyCors, { origin: true });
     this.app.register(fastifyHelmet);
     this.app.register(fastifyCompress);
@@ -79,6 +85,12 @@ class App {
 
   private initializeErrorHandling() {
     this.app.setErrorHandler((error: FastifyError, request, reply) => {
+      if (error.statusCode === 429) {
+        return reply
+          .status(429)
+          .send({ error: true, message: 'Too many requests. You hit the rate limit! Slow down please!' });
+      }
+
       const status: number = error.statusCode ?? 500;
       const message: string = status === 500 ? 'Something went wrong' : error.message ?? 'Something went wrong';
 
